3ebb8bf2ca05612b44a1fcce8ce716a5a0587f8b
Andy Lin <andy.lin.2004@outlook.com>
2023-11-01 15:45:47 -0400

    q1 and q2 done

---
+++ q1.js
@@ -0,0 +1,8 @@
+let str1 = "bofa";
+let str2 = "bofa";
+
+if (str1 == str2) {
+    alert("str1 and str2 are equal");
+} else {
+    alert("str1 and str2 aren't equal");
+}
---
+++ q2.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b = 2;
+
+if (a == b) {
+    alert("a and b are equal");
+} else if (a < b) {
+    alert("a is less than b");
+} else {
+    alert("a is greater than b");
+}
